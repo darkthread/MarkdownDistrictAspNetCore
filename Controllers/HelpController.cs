229e0e8d5d228fb579e1e20398760d7f4a39b76e
Jeffrey Lee <darkthread@gmail.com>
2022-06-22 18:13:19 +0800

    Add project files.

---
+++ Controllers/HelpController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace MarkdownDistrictAspNetCore.Controllers
+{
+    public class HelpController : Controller
+    {
+        string MarkdownPath;
+        public HelpController(IHostEnvironment env)
+        {
+            MarkdownPath = Path.Combine(env.ContentRootPath, "HelpDoc");
+        }
+
+        public IActionResult Index(string path)
+        {
+            // Path validation
+            path = path ?? string.Empty;
+            if (!Regex.IsMatch(path, "^[A-Za-z/]*$"))
+                return Content($"Ivalid Path - {path}");
+            var mdPath = Path.Combine(MarkdownPath, path.Replace("/", "\\"));
+            if (!System.IO.File.Exists(mdPath + ".md"))
+            {
+                mdPath = mdPath.TrimEnd('/') + "/index";
+                if (!System.IO.File.Exists(mdPath + ".md"))
+                    return NotFound();
+                else
+                {
+                    if (!string.IsNullOrEmpty(path)) 
+                        path = path.TrimEnd('/') + "/";
+                    return Redirect($"~/Help/{path}index");
+                }
+            }
+            ViewBag.HtmlContent = Markdig.Markdown.ToHtml(System.IO.File.ReadAllText(mdPath + ".md"));
+            return View();
+        }
+    }
+}
---
+++ HelpDoc/index.md
@@ -0,0 +1,8 @@
+﻿BLAH 線上手冊
+============
+
+* [PDF 檔下載](demo.pdf)
+* 擷圖範例  
+  ![Logo](logo.png)
+* [其他文件](other)
+* [子資料夾](SubFolder/)
